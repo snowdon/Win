7f32ec6c8dcdeeda90a408403653e49053a38004
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-25 22:50:37 +0800

    Window-based application
    “Window-based application”. In the tutorial that I am learning at the
    moment, it requires “Window-based application”. Xcode 4.2 beta 4 does
    not have “window-based application”; so, I created a project with “Empty
    application”. Unfortunately, “Empty application” has no longer
    “MainWindow.xib”, which tutorial teacher said to modify something in it.
    So, I googled to make “MainWindow.xib” visible and found this article.

---
+++ Win/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Win
+//
+//  Created by  Chuns on 11-8-25.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
